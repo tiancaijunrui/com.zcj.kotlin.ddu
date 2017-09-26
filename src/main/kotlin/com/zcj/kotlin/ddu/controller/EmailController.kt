a249086ca005c90058d45e4d6c920842c8cdddb3
zhacongjie <zhacongjie@qgutech.com>
2017-09-26 19:32:54 +0800

    邮件可以发送出去了 哈哈哈

--- src/main/kotlin/com/zcj/kotlin/ddu/controller/EmailController.kt
+++ src/main/kotlin/com/zcj/kotlin/ddu/controller/EmailController.kt
@@ -61,13 +61,17 @@ class EmailController {
     private fun doSendHtmlEmail(subject: String?, sendHtml: String?, receiveUserList: List<String>, file: File) = try {
         val sendUserName = InternetAddress("zhacongjie")
         val properties = Properties()
-        properties.put("mail.smtp.host", "smtp.126.com")
+        properties.put("mail.smtp.host", "smtp.qgutech.com")
         properties.put("mail.smtp.port", "25")
+        properties.put("mail.smtp.user","zhacongjie@qgutech.com")
+        properties.put("mail.smtp.auth","true")
+        properties.put("mail.smtp.debug","true")
 //            todo
 //        val auth = Authenticator()
-        val session = getDefaultInstance(properties, LoginAuthenticator("zcj984453536@126.com","zcj984453536??"))
+        val session = getDefaultInstance(properties, null)
+        session.debug = true
         val message = MimeMessage(session)
-        message.setFrom(InternetAddress("zcj984453536@126.com"))
+        message.setFrom(InternetAddress("zhacongjie@qgutech.com"))
         val address = arrayOfNulls<InternetAddress>(receiveUserList.size)
         for (ex in receiveUserList.indices){
             val exAddress = InternetAddress(receiveUserList[ex])
@@ -88,7 +92,7 @@ class EmailController {
         message.setContent(multiPart)
         message.saveChanges()
         val transport = session.getTransport("smtp")
-        transport.connect(properties.get("mail.smtp.host").toString(),25,"zcj984453536@126.com","zcj19921123??")
+        transport.connect(properties.get("mail.smtp.host").toString(),25,"zhacongjie@qgutech.com","qgutech.123")
         transport.sendMessage(message,message.allRecipients)
         transport.close()
     } finally {
@@ -97,7 +101,7 @@ class EmailController {
 
     private fun getReceiveUserList(): List<String> {
         val list = ArrayList<String>()
-        list.add("zhacongjie@qgutech.com")
+//        list.add("zhacongjie@qgutech.com")
         list.add("984453536@qq.com")
         return list
     }
