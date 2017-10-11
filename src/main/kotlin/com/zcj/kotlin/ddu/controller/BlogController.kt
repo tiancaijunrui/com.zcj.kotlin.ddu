d0e3debfb1a010f53cf90fa2ba11d1d0e133f3e6
zhacongjie <zhacongjie@qgutech.com>
2017-10-12 00:55:54 +0800

    获取博客地址下的文件名称

--- src/main/kotlin/com/zcj/kotlin/ddu/controller/BlogController.kt
+++ src/main/kotlin/com/zcj/kotlin/ddu/controller/BlogController.kt
@@ -1,18 +1,41 @@
 package com.zcj.kotlin.ddu.controller
 
+import com.zcj.kotlin.ddu.domain.Blog
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
+import java.io.File
+import javax.annotation.Resource
 
 /**
  * @Since2017/9/30 ZhaCongJie@HF
  */
 @Controller
-class BlogController{
+class BlogController {
+    @Autowired
+    private val env: Environment? = null
+
     @GetMapping("blog/index")
     @ResponseBody
-    fun index():ModelAndView{
+    fun index(): ModelAndView {
+        val blog :Blog = listFile()
         return ModelAndView("blog/index");
     }
+
+    private fun listFile(): Blog {
+        val path = "D:\\IdeaProjects\\blog\\source\\_posts"
+        val directory = File(path)
+        val blog = Blog()
+        if (directory.isDirectory) {
+            val fileList = directory.listFiles()
+            val fileNameList: ArrayList<String> = blog.fileNameList
+            fileList.mapTo(fileNameList) { it.name }
+        }
+        return blog
+    }
 }
+
--- src/main/kotlin/com/zcj/kotlin/ddu/domain/Blog.kt
+++ src/main/kotlin/com/zcj/kotlin/ddu/domain/Blog.kt
@@ -3,9 +3,10 @@ package com.zcj.kotlin.ddu.domain
 /**
  * @Since2017/9/30 ZhaCongJie@HF
  */
-class Bolg {
+class Blog {
     var title: String = ""
     var content: String = ""
     var tags: String = ""
     var categories: String = ""
+    var fileNameList: ArrayList<String> = ArrayList<String>()
 }
