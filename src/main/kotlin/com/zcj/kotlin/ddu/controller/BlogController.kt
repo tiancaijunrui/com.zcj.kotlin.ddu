4039443809b942d6a8a47d6e28c0d4b0113a6760
zhacongjie <zhacongjie@qgutech.com>
2017-10-16 20:10:09 +0800

    监听保存组合按键

--- src/main/kotlin/com/zcj/kotlin/ddu/controller/BlogController.kt
+++ src/main/kotlin/com/zcj/kotlin/ddu/controller/BlogController.kt
@@ -2,16 +2,14 @@ package com.zcj.kotlin.ddu.controller
 
 import com.sun.org.apache.xpath.internal.operations.Mod
 import com.zcj.kotlin.ddu.domain.Blog
+import com.zcj.kotlin.ddu.domain.JsonResult
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.core.env.get
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.ModelAndView
 import java.io.BufferedReader
 import java.io.File
@@ -24,6 +22,7 @@ import javax.servlet.http.HttpServletRequest
  * @Since2017/9/30 ZhaCongJie@HF
  */
 @Controller
+@RestController
 class BlogController {
     @Autowired
     private val env: Environment? = null
@@ -40,20 +39,28 @@ class BlogController {
     @ResponseBody
     fun editFileName(@PathVariable fileName: String, model: Model, request: HttpServletRequest): ModelAndView {
         val path = env?.getProperty("blog.physical.path") + env?.getProperty("blog.relative.path")
-        val filePath: String = path + "\\" + fileName+".md"
+        val filePath: String = path + "\\" + fileName + ".md"
         val file: File = File(filePath)
         val inputStreamReader: InputStreamReader = InputStreamReader(FileInputStream(file))
         val br = BufferedReader(inputStreamReader)
-        var line: String? = br.readLine()
-        val content: ArrayList<String> = ArrayList<String>()
+        var line: String? = br.readLine().trim()
+        val content: StringBuilder = StringBuilder()
         while (line != null) {
-            content.add(line)
+            line += "\r\n"
+            content.append(line)
             line = br.readLine()
+            println(line)
         }
         br.close()
         inputStreamReader.close()
-        model.set("content", content)
+        model.set("content", content.toString().trim())
         return ModelAndView("blog/edit")
+    }
+
+    @PostMapping("blog/save/{fileName}")
+    fun save(@PathVariable fileName: String, model: Model, request: HttpServletRequest): JsonResult {
+        val jsonResult = JsonResult();
+        return jsonResult;
     }
 
     private fun listFile(): Blog {
--- src/main/resources/templates/blog/edit.ftl
+++ src/main/resources/templates/blog/edit.ftl
@@ -2,24 +2,21 @@
 <!DOCTYPE html>
 <html lang="zh">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8"/>
     <title>Simple example - Editor.md examples</title>
-    <link rel="stylesheet" href="${staticPerfix!}/examples/css/style.css" />
-    <link rel="stylesheet" href="${staticPerfix!}/css/editormd.css" />
-    <link rel="shortcut icon" href="https://pandao.github.io/editor.md/favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="${staticPerfix!}/examples/css/style.css"/>
+    <link rel="stylesheet" href="${staticPerfix!}/css/editormd.css"/>
+    <link rel="shortcut icon" href="https://pandao.github.io/editor.md/favicon.ico" type="image/x-icon"/>
 </head>
 <body>
 <div id="layout">
     <header>
-        <h1>Simple example</h1>
+        <h1>${fileName!}</h1>
     </header>
     <div id="test-editormd"><textarea style="display:none;">
-        <#if content??>
-            <#list content as line>
-                ${line!}
-
-            </#list>
-        </#if>
+    <#if content??>
+${content!}
+    </#if>
     </textarea>
     </div>
 </div>
@@ -28,23 +25,25 @@
 <script type="text/javascript">
     var testEditor;
 
-    $(function() {
+    $(function () {
         testEditor = editormd("test-editormd", {
-            width   : "90%",
-            height  : 640,
-            syncScrolling : "single",
-            path    : "${staticPerfix!}/lib/"
+            width: "90%",
+            height: 640,
+            syncScrolling: "single",
+            path: "${staticPerfix!}/lib/"
         });
+        document.onkeydown = function(e) {
+            var keyCode = e.keyCode || e.which || e.charCode;
+            var ctrlKey = e.ctrlKey || e.metaKey;
+            if(ctrlKey && keyCode == 83) {
+                $.post("/blog/save/${fileName!}",{content:$("#test-editormd").find("textarea").val()},function (data) {
+                    debugger;
+                })
+            }
+            e.preventDefault();
+            return false;
+        }
 
-        /*
-        // or
-        testEditor = editormd({
-            id      : "test-editormd",
-            width   : "90%",
-            height  : 640,
-            path    : "../lib/"
-        });
-        */
     });
 </script>
 </body>
